4e8242a9e6100efae3c32abcc419325c2702dce8
David Sanders <davesque@gmail.com>
2017-05-06 14:28:26 -0600

    Update rest_framework_simplejwt.authentication

--- rest_framework_simplejwt/authentication.py
+++ rest_framework_simplejwt/authentication.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth import get_user_model
-from django.utils.translation import ugettext_lazy as _
 from django.utils.six import text_type
+from django.utils.translation import ugettext_lazy as _
 from jose import jwt
 from rest_framework import HTTP_HEADER_ENCODING
 from rest_framework.authentication import BaseAuthentication
@@ -20,6 +20,10 @@ User = get_user_model()
 
 
 class JWTAuthentication(BaseAuthentication):
+    """
+    An authentication plugin that authenticates requests through a JSON web
+    token provided in a request header.
+    """
     www_authenticate_realm = 'api'
 
     def authenticate(self, request):
@@ -43,6 +47,9 @@ class JWTAuthentication(BaseAuthentication):
         )
 
     def get_header(self, request):
+        """
+        Extracts a header containing a JSON web token from the given request.
+        """
         header = request.META.get('HTTP_AUTHORIZATION')
 
         if isinstance(header, text_type):
@@ -52,29 +59,46 @@ class JWTAuthentication(BaseAuthentication):
         return header
 
     def get_token(self, header):
+        """
+        Extracts a JSON web token from the given header.
+        """
         parts = header.split()
 
         if parts[0] != AUTH_HEADER_TYPE_BYTES:
+            # Assume the header does not contain a JSON web token
             return None
 
         if len(parts) != 2:
-            raise AuthenticationFailed(_('Authorization header is invalid.'))
+            raise AuthenticationFailed(
+                _('Authorization header must contain two space-delimited values.'),
+            )
 
         return parts[1]
 
     def get_payload(self, token):
+        """
+        Extracts a data payload from the given JSON web token.
+        """
         try:
             return jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
         except TypeError:
             raise AuthenticationFailed(_('Token is invalid.'))
 
     def get_user_id(self, payload):
+        """
+        Extracts a recognizable user identifier from the given data payload
+        object.
+        """
         try:
             return payload[PAYLOAD_ID_FIELD]
         except KeyError:
-            raise AuthenticationFailed(_('Token contained no user identification.'))
+            raise AuthenticationFailed(_('Token contained no recognizable user identification.'))
 
     def get_user(self, user_id):
+        """
+        Attempts to find and return a user record with the given user
+        identifier.
+        """
         try:
             user = User.objects.get(**{USER_ID_FIELD: user_id})
         except User.DoesNotExist:
