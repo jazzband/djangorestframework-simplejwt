8a50406c3d2cd630c9021472b6cadae49ec3dc8f
David Sanders <davesque@gmail.com>
2017-05-06 22:56:00 -0600

    Older Django version support

--- tests/urls.py
+++ tests/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^api/', include('rest_framework_simplejwt.urls')),
+    url(r'^api/', include('rest_framework_simplejwt.urls', namespace='rest_framework_simplejwt')),
 
     url(r'^test-view/$', views.test_view, name='test_view'),
 ]
