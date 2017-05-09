f2611ad1ae596eb555998d9b0f1663e3276c1d63
David Sanders <davesque@gmail.com>
2017-05-08 21:53:02 -0600

    Fix isort error

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-import unittest
+from unittest import TestCase
 
 from rest_framework_simplejwt.utils import datetime_to_epoch
 
 
-class TestDatetimeToEpoch(unittest.TestCase):
+class TestDatetimeToEpoch(TestCase):
     def test_it_should_return_the_correct_values(self):
         self.assertEqual(datetime_to_epoch(datetime(year=1970, month=1, day=1)), 0)
         self.assertEqual(datetime_to_epoch(datetime(year=1970, month=1, day=1, second=1)), 1)
