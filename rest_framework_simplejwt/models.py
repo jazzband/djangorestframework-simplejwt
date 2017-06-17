b771a718da90541889082469562af36d51e2575b
David Sanders <davesque@gmail.com>
2017-06-16 18:03:40 -0600

    Add rest_framework_simplejwt.models and tests

---
+++ rest_framework_simplejwt/models.py
@@ -0,0 +1,112 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth import models as auth_models
+from django.db.models.manager import EmptyManager
+from django.utils.deprecation import CallableFalse, CallableTrue
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.functional import cached_property
+
+from .settings import api_settings
+
+
+@python_2_unicode_compatible
+class TokenUser(object):
+    """
+    A dummy user class modeled after django.contrib.auth.models.AnonymousUser.
+    Used in conjunction with the `JWTTokenUserAuthentication` backend to
+    implement single sign-on functionality across services which share the same
+    secret key.  `JWTTokenUserAuthentication` will return an instance of this
+    class instead of a `User` model instance.  Instances of this class act as
+    stateless user objects which are built from validated token payloads.
+    """
+    username = ''
+
+    # User is always active since SimpleJWT will never issue a token for an
+    # inactive user
+    is_active = True
+
+    _groups = EmptyManager(auth_models.Group)
+    _user_permissions = EmptyManager(auth_models.Permission)
+
+    def __init__(self, token_payload):
+        self.token_payload = token_payload
+
+    def __getattr__(self, name):
+        try:
+            return self.token_payload[name]
+        except KeyError:
+            raise AttributeError("'{}' object has no attirbute '{}'".format(self.__class__.__name__, name))
+
+    def __str__(self):
+        return 'TokenUser {}'.format(self.id)
+
+    @cached_property
+    def id(self):
+        return self.token_payload[api_settings.PAYLOAD_ID_FIELD]
+
+    @cached_property
+    def pk(self):
+        return self.id
+
+    @cached_property
+    def is_staff(self):
+        return self.token_payload.get('is_staff', False)
+
+    @cached_property
+    def is_superuser(self):
+        return self.token_payload.get('is_superuser', False)
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def save(self):
+        raise NotImplementedError('Token users have no DB representation.')
+
+    def delete(self):
+        raise NotImplementedError('Token users have no DB representation.')
+
+    def set_password(self, raw_password):
+        raise NotImplementedError('Token users have no DB representation.')
+
+    def check_password(self, raw_password):
+        raise NotImplementedError('Token users have no DB representation.')
+
+    @property
+    def groups(self):
+        return self._groups
+
+    @property
+    def user_permissions(self):
+        return self._user_permissions
+
+    def get_group_permissions(self, obj=None):
+        return set()
+
+    def get_all_permissions(self, obj=None):
+        return set()
+
+    def has_perm(self, perm, obj=None):
+        return False
+
+    def has_perms(self, perm_list, obj=None):
+        return False
+
+    def has_module_perms(self, module):
+        return False
+
+    @property
+    def is_anonymous(self):
+        return CallableFalse
+
+    @property
+    def is_authenticated(self):
+        return CallableTrue
+
+    def get_username(self):
+        return self.username
---
+++ tests/test_models.py
@@ -0,0 +1,109 @@
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from django.utils import six
+from rest_framework_simplejwt.models import TokenUser
+from rest_framework_simplejwt.settings import api_settings
+
+
+class TestTokenUser(TestCase):
+    def setUp(self):
+        self.test_payload = {
+            api_settings.PAYLOAD_ID_FIELD: 42,
+            'some_other_stuff': 'arstarst',
+        }
+        self.user = TokenUser(self.test_payload)
+
+    def test_username(self):
+        self.assertEqual(self.user.username, '')
+
+    def test_is_active(self):
+        self.assertTrue(self.user.is_active)
+
+    def test_getattr(self):
+        self.assertEqual(self.user.some_other_stuff, 'arstarst')
+
+        with self.assertRaises(AttributeError):
+            self.user.oienarsoitenars
+
+    def test_str(self):
+        self.assertEqual(six.text_type(self.user), 'TokenUser 42')
+
+    def test_id(self):
+        self.assertEqual(self.user.id, 42)
+
+    def test_pk(self):
+        self.assertEqual(self.user.pk, 42)
+
+    def test_is_staff(self):
+        payload = {api_settings.PAYLOAD_ID_FIELD: 42}
+        user = TokenUser(payload)
+
+        self.assertFalse(user.is_staff)
+
+        payload['is_staff'] = True
+        user = TokenUser(payload)
+
+        self.assertTrue(user.is_staff)
+
+    def test_is_superuser(self):
+        payload = {api_settings.PAYLOAD_ID_FIELD: 42}
+        user = TokenUser(payload)
+
+        self.assertFalse(user.is_superuser)
+
+        payload['is_superuser'] = True
+        user = TokenUser(payload)
+
+        self.assertTrue(user.is_superuser)
+
+    def test_eq(self):
+        user1 = TokenUser({api_settings.PAYLOAD_ID_FIELD: 1})
+        user2 = TokenUser({api_settings.PAYLOAD_ID_FIELD: 2})
+        user3 = TokenUser({api_settings.PAYLOAD_ID_FIELD: 1})
+
+        self.assertNotEqual(user1, user2)
+        self.assertEqual(user1, user3)
+
+    def test_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            self.user.save()
+
+        with self.assertRaises(NotImplementedError):
+            self.user.delete()
+
+        with self.assertRaises(NotImplementedError):
+            self.user.set_password('arst')
+
+        with self.assertRaises(NotImplementedError):
+            self.user.check_password('arst')
+
+    def test_groups(self):
+        self.assertFalse(self.user.groups.exists())
+
+    def test_user_permissions(self):
+        self.assertFalse(self.user.user_permissions.exists())
+
+    def test_get_group_permissions(self):
+        self.assertEqual(len(self.user.get_group_permissions()), 0)
+
+    def test_get_all_permissions(self):
+        self.assertEqual(len(self.user.get_all_permissions()), 0)
+
+    def test_has_perm(self):
+        self.assertFalse(self.user.has_perm('test_perm'))
+
+    def test_has_perms(self):
+        self.assertFalse(self.user.has_perm(['test_perm']))
+
+    def test_has_module_perms(self):
+        self.assertFalse(self.user.has_module_perms('test_module'))
+
+    def test_is_anonymous(self):
+        self.assertFalse(self.user.is_anonymous)
+
+    def test_is_authenticated(self):
+        self.assertTrue(self.user.is_authenticated)
+
+    def test_get_username(self):
+        self.assertEqual(self.user.get_username(), '')
