f49bed4b1e4a6f16d2f0c4948c34931aa31efb08
David Sanders <davesque@gmail.com>
2017-06-19 00:09:12 -0600

    Add rest_framework_simplejwt.utils.format_lazy

--- rest_framework_simplejwt/utils.py
+++ rest_framework_simplejwt/utils.py
@@ -1,5 +1,14 @@
 from calendar import timegm
 
+from django.utils import six
+from django.utils.functional import lazy
+
 
 def datetime_to_epoch(dt):
     return timegm(dt.utctimetuple())
+
+
+def format_lazy(s, *args, **kwargs):
+    return s.format(*args, **kwargs)
+
+format_lazy = lazy(format_lazy, six.text_type)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from unittest import TestCase
 
-from rest_framework_simplejwt.utils import datetime_to_epoch
+from django.utils import six
+
+from rest_framework_simplejwt.utils import datetime_to_epoch, format_lazy
 
 
 class TestDatetimeToEpoch(TestCase):
@@ -9,3 +11,11 @@ class TestDatetimeToEpoch(TestCase):
         self.assertEqual(datetime_to_epoch(datetime(year=1970, month=1, day=1)), 0)
         self.assertEqual(datetime_to_epoch(datetime(year=1970, month=1, day=1, second=1)), 1)
         self.assertEqual(datetime_to_epoch(datetime(year=2000, month=1, day=1)), 946684800)
+
+
+class TestFormatLazy(TestCase):
+    def test_it_should_work(self):
+        obj = format_lazy('{} {}', 'arst', 'zxcv')
+
+        self.assertNotIsInstance(obj, str)
+        self.assertEqual(six.text_type(obj), 'arst zxcv')
