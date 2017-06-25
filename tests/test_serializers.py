f5eb47ad7ab1429263b31ad2cc16ac70e492505f
David Sanders <davesque@gmail.com>
2017-06-24 20:13:25 -0600

    Add TestTokenObtainSerializer

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -4,11 +4,62 @@ from datetime import datetime, timedelta
 
 from django.test import TestCase
 from rest_framework_simplejwt.serializers import (
-    TokenObtainSlidingSerializer, TokenRefreshSlidingSerializer
+    TokenObtainSerializer, TokenObtainSlidingSerializer,
+    TokenRefreshSlidingSerializer
 )
 from rest_framework_simplejwt.settings import api_settings
 from rest_framework_simplejwt.state import User
 from rest_framework_simplejwt.tokens import SlidingToken
+
+
+class TestTokenObtainSerializer(TestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'test_password'
+
+        self.user = User.objects.create_user(
+            username=self.username,
+            password=self.password,
+        )
+
+    def test_it_should_not_validate_if_any_fields_missing(self):
+        s = TokenObtainSerializer(data={})
+        self.assertFalse(s.is_valid())
+        self.assertIn(s.username_field, s.errors)
+        self.assertIn('password', s.errors)
+
+        s = TokenObtainSerializer(data={
+            TokenObtainSerializer.username_field: 'oieanrst',
+        })
+        self.assertFalse(s.is_valid())
+        self.assertIn('password', s.errors)
+
+        s = TokenObtainSerializer(data={
+            'password': 'oieanrst',
+        })
+        self.assertFalse(s.is_valid())
+        self.assertIn(s.username_field, s.errors)
+
+    def test_it_should_not_validate_if_user_not_found(self):
+        s = TokenObtainSerializer(data={
+            TokenObtainSerializer.username_field: 'missing',
+            'password': 'pass',
+        })
+
+        self.assertFalse(s.is_valid())
+        self.assertIn('non_field_errors', s.errors)
+
+    def test_it_should_not_validate_if_user_not_active(self):
+        self.user.is_active = False
+        self.user.save()
+
+        s = TokenObtainSerializer(data={
+            TokenObtainSerializer.username_field: self.username,
+            'password': self.password,
+        })
+
+        self.assertFalse(s.is_valid())
+        self.assertIn('non_field_errors', s.errors)
 
 
 class TestTokenObtainSlidingSerializer(TestCase):
@@ -20,45 +71,6 @@ class TestTokenObtainSlidingSerializer(TestCase):
             username=self.username,
             password=self.password,
         )
-
-    def test_it_should_not_validate_if_any_fields_missing(self):
-        s = TokenObtainSlidingSerializer(data={})
-        self.assertFalse(s.is_valid())
-        self.assertIn(s.username_field, s.errors)
-        self.assertIn('password', s.errors)
-
-        s = TokenObtainSlidingSerializer(data={
-            TokenObtainSlidingSerializer.username_field: 'oieanrst',
-        })
-        self.assertFalse(s.is_valid())
-        self.assertIn('password', s.errors)
-
-        s = TokenObtainSlidingSerializer(data={
-            'password': 'oieanrst',
-        })
-        self.assertFalse(s.is_valid())
-        self.assertIn(s.username_field, s.errors)
-
-    def test_it_should_not_validate_if_user_not_found(self):
-        s = TokenObtainSlidingSerializer(data={
-            TokenObtainSlidingSerializer.username_field: 'missing',
-            'password': 'pass',
-        })
-
-        self.assertFalse(s.is_valid())
-        self.assertIn('non_field_errors', s.errors)
-
-    def test_it_should_not_validate_if_user_not_active(self):
-        self.user.is_active = False
-        self.user.save()
-
-        s = TokenObtainSlidingSerializer(data={
-            TokenObtainSlidingSerializer.username_field: self.username,
-            'password': self.password,
-        })
-
-        self.assertFalse(s.is_valid())
-        self.assertIn('non_field_errors', s.errors)
 
     def test_it_should_produce_a_json_web_token_when_valid(self):
         s = TokenObtainSlidingSerializer(data={
