e78f7ee1e5ebb579f9162b6245c7f68e4ad066c1
David Sanders <davesque@gmail.com>
2017-06-24 18:22:29 -0600

    Variable name refactors

--- rest_framework_simplejwt/tokens.py
+++ rest_framework_simplejwt/tokens.py
@@ -46,14 +46,14 @@ class Token(object):
     def __repr__(self):
         return repr(self.payload)
 
-    def __getitem__(self, name):
-        return self.payload[name]
+    def __getitem__(self, key):
+        return self.payload[key]
 
-    def __setitem__(self, name, value):
-        self.payload[name] = value
+    def __setitem__(self, key, value):
+        self.payload[key] = value
 
-    def __contains__(self, name):
-        return name in self.payload
+    def __contains__(self, key):
+        return key in self.payload
 
     def __str__(self):
         """
