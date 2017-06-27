d73adc216a2aa90055fcfb8a808c9673454b50c3
David Sanders <davesque@gmail.com>
2017-06-27 01:58:13 -0600

    Re-organize setting vars

--- rest_framework_simplejwt/settings.py
+++ rest_framework_simplejwt/settings.py
@@ -8,27 +8,26 @@ from rest_framework.settings import APISettings
 USER_SETTINGS = getattr(settings, 'SIMPLE_JWT', None)
 
 DEFAULTS = {
-    'AUTH_HEADER_TYPE': 'Bearer',
-
-    'USER_ID_FIELD': 'id',
-
-    'USER_ID_CLAIM': 'user_id',
-    'TOKEN_TYPE_CLAIM': 'token_type',
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 
     'SECRET_KEY': settings.SECRET_KEY,
 
+    'AUTH_HEADER_TYPE': 'Bearer',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASS': 'rest_framework_simplejwt.tokens.AccessToken',
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+
     # Undocumented settings.  Changing these may lead to unexpected behavior.
     # Make sure you know what you're doing.  These might become part of the
     # public API eventually but that would require some adjustments and better
     # documentation.
-    'AUTH_TOKEN_CLASS': 'rest_framework_simplejwt.tokens.AccessToken',
     'TOKEN_BACKEND_CLASS': 'rest_framework_simplejwt.backends.PythonJOSEBackend',
     'ALGORITHM': 'HS256',
 }
