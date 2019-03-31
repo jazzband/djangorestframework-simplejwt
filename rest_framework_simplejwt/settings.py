d1ce340296498ffdd2a6a7c081a0013be1536a77
Thomas Boni <thomas.boni@pikcio.com>
2019-03-31 19:44:14 +0200

    add authorization custom header name option

--- rest_framework_simplejwt/settings.py
+++ rest_framework_simplejwt/settings.py
@@ -19,6 +19,7 @@ DEFAULTS = {
     'SIGNING_KEY': settings.SECRET_KEY,
     'VERIFYING_KEY': None,
 
+    'AUTH_HEADER_NAME': 'Authorization',
     'AUTH_HEADER_TYPES': ('Bearer',),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
