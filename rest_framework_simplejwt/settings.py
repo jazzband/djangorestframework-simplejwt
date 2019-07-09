de7d249330a59bd7247a61c02a257c1f0de42a01
Samuli-Petrus Korhonen <samuli-petrus.korhonen@iki.fi>
2019-07-09 09:33:20 +0300

    Allow custom functions to get user / user_id

--- rest_framework_simplejwt/settings.py
+++ rest_framework_simplejwt/settings.py
@@ -18,6 +18,8 @@ DEFAULTS = {
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': settings.SECRET_KEY,
     'VERIFYING_KEY': None,
+    'USER_ID_TO_USER': None,
+    'USER_TO_USER_ID': None,
 
     'AUTH_HEADER_TYPES': ('Bearer',),
     'USER_ID_FIELD': 'id',
