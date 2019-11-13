14402b4c19cb4b358f6fb242183b09dd5851f081
SoungRyoul Kim <soungryoul.kim@deliveryhero.co.kr>
2019-11-13 16:25:12 +0900

    check vendor version and modify jti_hex max length

--- rest_framework_simplejwt/token_blacklist/migrations/0004_auto_20171017_2013.py
+++ rest_framework_simplejwt/token_blacklist/migrations/0004_auto_20171017_2013.py
@@ -1,13 +1,29 @@
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration):
+def is_vendor_less_than_mysql_innodb_56():
+    from django.db import connection
+    cursor = connection.cursor()
+    cursor.execute('SHOW VARIABLES LIKE "innodb_version"')
 
+    mysql_version = cursor.fetchone()
+
+    return mysql_version and mysql_version[1] < '5.7.0'
+
+
+class Migration(migrations.Migration):
     dependencies = [
         ('token_blacklist', '0003_auto_20171017_2007'),
     ]
 
     operations = [
+        migrations.AlterField(
+            model_name='outstandingtoken',
+            name='jti_hex',
+            field=models.CharField(unique=True, max_length=191),
+        )
+        if is_vendor_less_than_mysql_innodb_56()
+        else
         migrations.AlterField(
             model_name='outstandingtoken',
             name='jti_hex',
