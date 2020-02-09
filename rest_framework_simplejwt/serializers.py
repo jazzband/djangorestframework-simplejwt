d54a104285ae3e5127c46bdf4d95d93b31e4071f
Kianoosh Hooshmand <kianoosh.hooshmand@gmail.com>
2020-02-09 12:41:09 +0330

    Add Custom Username Field to TokenObtainSerializer

--- rest_framework_simplejwt/serializers.py
+++ rest_framework_simplejwt/serializers.py
@@ -19,6 +19,7 @@ class PasswordField(serializers.CharField):
 
 class TokenObtainSerializer(serializers.Serializer):
     username_field = User.USERNAME_FIELD
+    custom_username_field = api_settings.CUSTOM_USERNAME_FIELD
 
     default_error_messages = {
         'no_active_account': _('No active account found with the given credentials')
@@ -27,12 +28,16 @@ class TokenObtainSerializer(serializers.Serializer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields[self.username_field] = serializers.CharField()
+        self.fields[self.username] = serializers.CharField()
         self.fields['password'] = PasswordField()
+
+    @property
+    def username(self):
+        return self.custom_username_field if self.custom_username_field else self.username_field
 
     def validate(self, attrs):
         authenticate_kwargs = {
-            self.username_field: attrs[self.username_field],
+            self.username_field: attrs[self.username],
             'password': attrs['password'],
         }
         try:
--- rest_framework_simplejwt/settings.py
+++ rest_framework_simplejwt/settings.py
@@ -24,6 +24,7 @@ DEFAULTS = {
     'AUTH_HEADER_TYPES': ('Bearer',),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
+    'CUSTOM_USERNAME_FIELD': None,
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
