e7fd3634d0ec4581e908243a7c181b51cb3aa3b1
Mads Jensen <mje@inducks.org>
2020-04-16 11:35:52 +0200

    Removed unneeded TokenUser.__ne__ method.
    This method is not needed since Python 3 is the minimum requirement.

--- rest_framework_simplejwt/models.py
+++ rest_framework_simplejwt/models.py
@@ -51,9 +51,6 @@ class TokenUser:
     def __eq__(self, other):
         return self.id == other.id
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
     def __hash__(self):
         return hash(self.id)
 
