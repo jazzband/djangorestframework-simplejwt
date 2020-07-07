2db3a288fe6ebbefbb15119445dc19c3779da3ec
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2020-07-07 00:10:39 -0400

    Update migration for OutstandingToken to SET_NULL

--- rest_framework_simplejwt/token_blacklist/migrations/0007_auto_20171017_2214.py
+++ rest_framework_simplejwt/token_blacklist/migrations/0007_auto_20171017_2214.py
@@ -18,6 +18,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='outstandingtoken',
             name='user',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
         ),
     ]
