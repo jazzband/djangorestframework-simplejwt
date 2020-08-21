1b053bfe4db5fe5e3ca77e18f186cdf673e8bc93
Thomas Iovine <t_io@codesoup.net>
2020-08-21 14:53:24 +0200

    add configuration for user authentication (#279)
    * add configuration for user authentication.
    
    * Isort lint
    
    Co-authored-by: Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>

--- rest_framework_simplejwt/settings.py
+++ rest_framework_simplejwt/settings.py
@@ -24,6 +24,7 @@ DEFAULTS = {
     'AUTH_HEADER_TYPES': ('Bearer',),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
