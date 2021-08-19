0adaa35973b4839a55e044c8317ddbcaeaefd530
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-08-19 13:16:19 -0400

    Verify should check if token_blacklist installed

--- rest_framework_simplejwt/serializers.py
+++ rest_framework_simplejwt/serializers.py
@@ -145,7 +145,10 @@ class TokenVerifySerializer(serializers.Serializer):
     def validate(self, attrs):
         token = UntypedToken(attrs['token'])
 
-        if api_settings.BLACKLIST_AFTER_ROTATION:
+        if (
+            api_settings.BLACKLIST_AFTER_ROTATION
+            and 'rest_framework_simplejwt.token_blacklist' in settings.INSTALLED_APPS
+        ):
             jti = token.get(api_settings.JTI_CLAIM)
             if BlacklistedToken.objects.filter(token__jti=jti).exists():
                 raise ValidationError("Token is blacklisted")
