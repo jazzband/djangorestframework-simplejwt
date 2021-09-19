d9667ddbd1f590f59e452fcfbb7c030ae47d3d46
Mahmoud Adel <20120831@std.sci.cu.edu.eg>
2021-09-19 20:21:47 +0200

    Set BLACKLIST_AFTER_ROTATION by default to False at docs (#455)
    * Set BLACKLIST_AFTER_ROTATION to False in docs
    
    * Set BLACKLIST_AFTER_ROTATION default to False
    
    * Update test settings
    
    Co-authored-by: Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>

--- rest_framework_simplejwt/settings.py
+++ rest_framework_simplejwt/settings.py
@@ -13,7 +13,7 @@ DEFAULTS = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
