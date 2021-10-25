8312b15809fb2a4cfe6d6bb485ced962d8470020
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-10-25 09:58:17 +0100

    Updated controller output schemas

--- ninja_jwt/controller.py
+++ ninja_jwt/controller.py
@@ -5,22 +5,22 @@ from ninja_jwt import schema
 
 class TokenVerificationController(APIController):
     @route.post('/verify-token', response=schema.TokenVerifySerializer)
-    def verify_token(self, token):
+    def verify_token(self, token: str):
         verified_token = schema.TokenVerifySerializer(token=token)
         return verified_token
 
 
 class TokenBlackListController(APIController):
-    @route.post('/blacklist-token', response=schema.TokenVerifySerializer)
-    def blacklist_token(self, token):
+    @route.post('/blacklist-token', response=schema.TokenBlacklistSerializer)
+    def blacklist_token(self, token: str):
         verified_token = schema.TokenVerifySerializer(token=token)
         return verified_token
 
 
 class TokenObtainPairController(APIController):
-    @route.post('/obtain-token', response=schema.TokenObtainPairSerializer)
+    @route.post('/obtain-token', response=schema.TokenObtainPairOutput)
     def obtain_token(self, user: schema.TokenObtainPairSerializer):
-        return user.dict(exclude=set('password'))
+        return user.output_schema()
 
     @route.post('/refresh-token', response=schema.TokenRefreshSerializer)
     def refresh_token(self, refresh_token: str):
@@ -29,9 +29,9 @@ class TokenObtainPairController(APIController):
 
 
 class TokenObtainSlidingController(TokenObtainPairController):
-    @route.post('/obtain-token', response=schema.TokenObtainSlidingSerializer)
+    @route.post('/obtain-token', response=schema.TokenObtainSlidingOutput)
     def obtain_token(self, user: schema.TokenObtainSlidingSerializer):
-        return user.dict(exclude=set('password'))
+        return user.output_schema()
 
     @route.post('/refresh-token', response=schema.TokenRefreshSlidingSerializer)
     def refresh_token(self, refresh_token: str):
