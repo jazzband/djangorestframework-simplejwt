e5baf6a251bc6f1a97b4a71caf626385e668eb54
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-10-31 11:44:49 -0400

    Verify should check if token_blacklist installed (#450)
    * Verify should check if token_blacklist installed
    
    * Add missing import

--- rest_framework_simplejwt/serializers.py
+++ rest_framework_simplejwt/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.models import update_last_login
 from django.utils.translation import gettext_lazy as _
@@ -147,7 +148,10 @@ class TokenVerifySerializer(serializers.Serializer):
     def validate(self, attrs):
         token = UntypedToken(attrs['token'])
 
-        if api_settings.BLACKLIST_AFTER_ROTATION:
+        if (
+            api_settings.BLACKLIST_AFTER_ROTATION
+            and 'rest_framework_simplejwt.token_blacklist' in settings.INSTALLED_APPS
+        ):
             jti = token.get(api_settings.JTI_CLAIM)
             if BlacklistedToken.objects.filter(token__jti=jti).exists():
                 raise ValidationError("Token is blacklisted")
