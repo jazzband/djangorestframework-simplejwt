6dcd0505f2479cb161dab01323e12cef02eb33ef
Youngkwang Yang <immutable000@gmail.com>
2022-08-14 14:46:01 +0900

    Add classifiers

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
