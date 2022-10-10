6c03c4ea547ce973b16614247cc46cb41bb7223b
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2022-10-10 11:18:45 -0400

    Revert 605 (#629)

--- rest_framework_simplejwt/authentication.py
+++ rest_framework_simplejwt/authentication.py
@@ -89,7 +89,7 @@ class JWTAuthentication(authentication.BaseAuthentication):
         messages = []
         for AuthToken in api_settings.AUTH_TOKEN_CLASSES:
             try:
-                return AuthToken(raw_token, api_settings.VERIFYING_KEY is not None)
+                return AuthToken(raw_token)
             except TokenError as e:
                 messages.append(
                     {
