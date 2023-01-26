d8e7ed824636b1a9d6330cab4c2b6ca2b6727c7e
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-26 14:51:40 +0100

    update config and remove en files

--- inlang.config.js
+++ inlang.config.js
@@ -1,5 +1,3 @@
-
-
 // filename: inlang.config.js
 
 export async function defineConfig(env) {
@@ -11,15 +9,38 @@ export async function defineConfig(env) {
   // most plugins require additional config, read the plugins documentation
   // for the required config and correct usage.
   const pluginConfig = {
-    pathPattern: "./rest_framework_simplejwt/locale/{language}/LC_MESSAGES/django.po",
+    pathPattern:
+      "./rest_framework_simplejwt/locale/{language}/LC_MESSAGES/django.po",
+    referenceResourcePath: null,
   };
 
   return {
     referenceLanguage: "en",
-    languages: ["cs","de","es","es_AR","es_CL","fa_IR","fr","id_ID","it_IT","ko_KR","nl_NL","pl_PL","pt_BR","ro","ru_RU","sl","sv","tr","uk_UA","zh_Hans"],
+    languages: [
+      "cs",
+      "de",
+      "es",
+      "es_AR",
+      "es_CL",
+      "fa_IR",
+      "fr",
+      "id_ID",
+      "it_IT",
+      "ko_KR",
+      "nl_NL",
+      "pl_PL",
+      "pt_BR",
+      "ro",
+      "ru_RU",
+      "sl",
+      "sv",
+      "tr",
+      "uk_UA",
+      "zh_Hans",
+    ],
     readResources: (args) =>
-    plugin.readResources({ ...args, ...env, pluginConfig }),
-  writeResources: (args) =>
-    plugin.writeResources({ ...args, ...env, pluginConfig }),
+      plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) =>
+      plugin.writeResources({ ...args, ...env, pluginConfig }),
   };
 }
