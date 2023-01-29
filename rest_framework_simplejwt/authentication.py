9f2914d21a3d10ebcf88d36097a7b984a38910b3
abdut7 <34902775+abdut7@users.noreply.github.com>
2023-01-30 02:54:02 +0530

    Update authentication.py

--- rest_framework_simplejwt/authentication.py
+++ rest_framework_simplejwt/authentication.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from rest_framework import HTTP_HEADER_ENCODING, authentication
+import json
 
 from .exceptions import AuthenticationFailed, InvalidToken, TokenError
 from .settings import api_settings
