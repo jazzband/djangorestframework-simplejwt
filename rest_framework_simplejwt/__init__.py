f4458aa82f0a1d40fc4abc1192cd03640a714565
Caellion <administrator@michalpol.pl>
2023-04-25 08:53:35 +0200

    Should fix upstream's jazzband#684
    Co-Authored-By: nielsuit227

--- rest_framework_simplejwt/__init__.py
+++ rest_framework_simplejwt/__init__.py
@@ -1,7 +1,12 @@
-from pkg_resources import DistributionNotFound, get_distribution
+import sys
 
 try:
-    __version__ = get_distribution("djangorestframework_simplejwt").version
+    if sys.version_info >= (3, 8):
+        from importlib import metadata
+    else:
+        import importlib_metadata as metadata
+    
+    __version__ =  metadata.version("djangorestframework_simplejwt")
 except DistributionNotFound:
     # package is not installed
     __version__ = None
