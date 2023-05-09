303aa7f4f543e37b0d14be28c8d6240625dfda85
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-05-09 11:40:22 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -1,47 +1,17 @@
-// filename: inlang.config.js
-
 export async function defineConfig(env) {
-  // importing a plugin
-  const plugin = await env.$import(
-    "https://cdn.jsdelivr.net/gh/jannesblobel/inlang-plugin-po@1/dist/index.js"
-  );
+	const { default: pluginJson } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/jannesblobel/inlang-plugin-po@1/dist/index.js'
+	);
 
-  // most plugins require additional config, read the plugins documentation
-  // for the required config and correct usage.
-  const pluginConfig = {
-    pathPattern:
-      "./rest_framework_simplejwt/locale/{language}/LC_MESSAGES/django.po",
-    referenceResourcePath: null,
-  };
+	const { default: standardLintRules } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
+	);
 
-  return {
-    referenceLanguage: "en",
-    languages: [
-      "en",
-      "cs",
-      "de",
-      "es",
-      "es_AR",
-      "es_CL",
-      "fa_IR",
-      "fr",
-      "id_ID",
-      "it_IT",
-      "ko_KR",
-      "nl_NL",
-      "pl_PL",
-      "pt_BR",
-      "ro",
-      "ru_RU",
-      "sl",
-      "sv",
-      "tr",
-      "uk_UA",
-      "zh_Hans",
-    ],
-    readResources: (args) =>
-      plugin.readResources({ ...args, ...env, pluginConfig }),
-    writeResources: (args) =>
-      plugin.writeResources({ ...args, ...env, pluginConfig }),
-  };
+	return {
+		referenceLanguage: 'en',
+		plugins: [pluginJson({ 
+			pathPattern: './resources/{language}.json',
+			variableReferencePattern: ["{", "}"]
+		}), standardLintRules()]
+	};
 }
