437558f88cc1d32ef0097257a87fc16a4ccbf787
Caellion <administrator@michalpol.pl>
2023-07-03 08:08:08 +0200

    Use old code on Python <3.8
    as per @Andrew-Chen-Wang's request

--- rest_framework_simplejwt/__init__.py
+++ rest_framework_simplejwt/__init__.py
@@ -3,10 +3,11 @@ import sys
 try:
     if sys.version_info >= (3, 8):
         from importlib import metadata
+        __version__ = metadata.version("djangorestframework_simplejwt")
     else:
-        import importlib_metadata as metadata
-
-    __version__ = metadata.version("djangorestframework_simplejwt")
+        from pkg_resources import DistributionNotFound, get_distribution
+        __version__ = get_distribution("djangorestframework_simplejwt").version
+    
 except DistributionNotFound:
     # package is not installed
     __version__ = None
