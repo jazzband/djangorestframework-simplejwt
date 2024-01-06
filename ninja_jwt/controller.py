73c0ce18c883a4ab4dd5033af3fddd28886d3f23
Tochukwu <dlastdon4000@gmail.com>
2024-01-06 11:50:18 +0100

    Skip global auth (#57)
    * Global Auth Skip: set default JWT Controllers auth to None to skip global auth
    
    * 5.3.0
    
    * updated publish.yml

--- ninja_jwt/controller.py
+++ ninja_jwt/controller.py
@@ -1,4 +1,4 @@
-import django
+from asgiref.sync import sync_to_async
 from ninja import Schema
 from ninja_extra import ControllerBase, api_controller, http_post
 from ninja_extra.permissions import AllowAny
@@ -6,7 +6,7 @@ from ninja_extra.permissions import AllowAny
 from ninja_jwt.schema_control import SchemaControl
 from ninja_jwt.settings import api_settings
 
-exports = [
+__all__ = [
     "TokenVerificationController",
     "TokenBlackListController",
     "TokenObtainPairController",
@@ -14,21 +14,14 @@ exports = [
     "TokenObtainSlidingController",
     "NinjaJWTDefaultController",
     "NinjaJWTSlidingController",
+    "AsyncTokenVerificationController",
+    "AsyncTokenBlackListController",
+    "AsyncTokenObtainPairController",
+    "AsyncTokenObtainSlidingController",
+    "AsyncTokenObtainSlidingController",
+    "AsyncNinjaJWTDefaultController",
+    "AsyncNinjaJWTSlidingController",
 ]
-
-if not django.VERSION < (3, 1):
-    exports += [
-        "AsyncTokenVerificationController",
-        "AsyncTokenBlackListController",
-        "AsyncTokenObtainPairController",
-        "AsyncTokenObtainSlidingController",
-        "AsyncTokenObtainSlidingController",
-        "AsyncNinjaJWTDefaultController",
-        "AsyncNinjaJWTSlidingController",
-    ]
-
-__all__ = exports
-
 
 schema = SchemaControl(api_settings)
 
@@ -99,14 +92,14 @@ class TokenObtainSlidingController(TokenObtainPairController):
         return refresh_token.to_response_schema()
 
 
-@api_controller("/token", permissions=[AllowAny], tags=["token"])
+@api_controller("/token", permissions=[AllowAny], tags=["token"], auth=None)
 class NinjaJWTDefaultController(TokenVerificationController, TokenObtainPairController):
     """NinjaJWT Default controller for obtaining and refreshing tokens"""
 
     auto_import = False
 
 
-@api_controller("/token", permissions=[AllowAny], tags=["token"])
+@api_controller("/token", permissions=[AllowAny], tags=["token"], auth=None)
 class NinjaJWTSlidingController(
     TokenVerificationController, TokenObtainSlidingController
 ):
@@ -118,84 +111,84 @@ class NinjaJWTSlidingController(
     auto_import = False
 
 
-if not django.VERSION < (3, 1):
-    from asgiref.sync import sync_to_async
+class AsyncTokenVerificationController(TokenVerificationController):
+    @http_post(
+        "/verify",
+        response={200: Schema},
+        url_name="token_verify",
+    )
+    async def verify_token(self, token: schema.verify_schema):
+        return token.to_response_schema()
 
-    class AsyncTokenVerificationController(TokenVerificationController):
-        @http_post(
-            "/verify",
-            response={200: Schema},
-            url_name="token_verify",
-        )
-        async def verify_token(self, token: schema.verify_schema):
-            return token.to_response_schema()
 
-    class AsyncTokenBlackListController(TokenBlackListController):
-        auto_import = False
+class AsyncTokenBlackListController(TokenBlackListController):
+    auto_import = False
 
-        @http_post(
-            "/blacklist",
-            response={200: Schema},
-            url_name="token_blacklist",
-        )
-        async def blacklist_token(self, refresh: schema.blacklist_schema):
-            return refresh.to_response_schema()
+    @http_post(
+        "/blacklist",
+        response={200: Schema},
+        url_name="token_blacklist",
+    )
+    async def blacklist_token(self, refresh: schema.blacklist_schema):
+        return refresh.to_response_schema()
 
-    class AsyncTokenObtainPairController(TokenObtainPairController):
-        @http_post(
-            "/pair",
-            response=schema.obtain_pair_schema.get_response_schema(),
-            url_name="token_obtain_pair",
-        )
-        async def obtain_token(self, user_token: schema.obtain_pair_schema):
-            await sync_to_async(user_token.check_user_authentication_rule)()
-            return user_token.to_response_schema()
 
-        @http_post(
-            "/refresh",
-            response=schema.obtain_pair_refresh_schema.get_response_schema(),
-            url_name="token_refresh",
-        )
-        async def refresh_token(self, refresh_token: schema.obtain_pair_refresh_schema):
-            refresh = await sync_to_async(refresh_token.to_response_schema)()
-            return refresh
+class AsyncTokenObtainPairController(TokenObtainPairController):
+    @http_post(
+        "/pair",
+        response=schema.obtain_pair_schema.get_response_schema(),
+        url_name="token_obtain_pair",
+    )
+    async def obtain_token(self, user_token: schema.obtain_pair_schema):
+        await sync_to_async(user_token.check_user_authentication_rule)()
+        return user_token.to_response_schema()
 
-    class AsyncTokenObtainSlidingController(TokenObtainSlidingController):
-        @http_post(
-            "/sliding",
-            response=schema.obtain_sliding_schema.get_response_schema(),
-            url_name="token_obtain_sliding",
-        )
-        async def obtain_token(self, user_token: schema.obtain_sliding_schema):
-            await sync_to_async(user_token.check_user_authentication_rule)()
-            return user_token.to_response_schema()
+    @http_post(
+        "/refresh",
+        response=schema.obtain_pair_refresh_schema.get_response_schema(),
+        url_name="token_refresh",
+    )
+    async def refresh_token(self, refresh_token: schema.obtain_pair_refresh_schema):
+        refresh = await sync_to_async(refresh_token.to_response_schema)()
+        return refresh
 
-        @http_post(
-            "/sliding/refresh",
-            response=schema.obtain_sliding_refresh_schema.get_response_schema(),
-            url_name="token_refresh_sliding",
-        )
-        async def refresh_token(
-            self, refresh_token: schema.obtain_sliding_refresh_schema
-        ):
-            refresh = await sync_to_async(refresh_token.to_response_schema)()
-            return refresh
 
-    @api_controller("/token", permissions=[AllowAny], tags=["token"])
-    class AsyncNinjaJWTDefaultController(
-        AsyncTokenVerificationController, AsyncTokenObtainPairController
-    ):
-        """NinjaJWT Async Default controller for obtaining and refreshing tokens"""
+class AsyncTokenObtainSlidingController(TokenObtainSlidingController):
+    @http_post(
+        "/sliding",
+        response=schema.obtain_sliding_schema.get_response_schema(),
+        url_name="token_obtain_sliding",
+    )
+    async def obtain_token(self, user_token: schema.obtain_sliding_schema):
+        await sync_to_async(user_token.check_user_authentication_rule)()
+        return user_token.to_response_schema()
 
-        auto_import = False
+    @http_post(
+        "/sliding/refresh",
+        response=schema.obtain_sliding_refresh_schema.get_response_schema(),
+        url_name="token_refresh_sliding",
+    )
+    async def refresh_token(self, refresh_token: schema.obtain_sliding_refresh_schema):
+        refresh = await sync_to_async(refresh_token.to_response_schema)()
+        return refresh
 
-    @api_controller("/token", permissions=[AllowAny], tags=["token"])
-    class AsyncNinjaJWTSlidingController(
-        AsyncTokenVerificationController, AsyncTokenObtainSlidingController
-    ):
-        """
-        NinjaJWT Async Sliding controller for obtaining and refreshing tokens
-        Add 'ninja_jwt.tokens.SlidingToken' in AUTH_TOKEN_CLASSES in Settings
-        """
 
-        auto_import = False
+@api_controller("/token", permissions=[AllowAny], tags=["token"], auth=None)
+class AsyncNinjaJWTDefaultController(
+    AsyncTokenVerificationController, AsyncTokenObtainPairController
+):
+    """NinjaJWT Async Default controller for obtaining and refreshing tokens"""
+
+    auto_import = False
+
+
+@api_controller("/token", permissions=[AllowAny], tags=["token"], auth=None)
+class AsyncNinjaJWTSlidingController(
+    AsyncTokenVerificationController, AsyncTokenObtainSlidingController
+):
+    """
+    NinjaJWT Async Sliding controller for obtaining and refreshing tokens
+    Add 'ninja_jwt.tokens.SlidingToken' in AUTH_TOKEN_CLASSES in Settings
+    """
+
+    auto_import = False
