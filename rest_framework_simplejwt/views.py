9946355539e9f0a7750f2a1b60e2091048cbf29c
Logan Davidson <iamlogandavidson@gmail.com>
2024-07-16 19:14:57 +0100

    Add a type hint for `serializer_class`
    Add a type hint that will tell type checkers like mypy that `serializer_class` should be either `Type[serializers.Serializer]` or `None`.

--- rest_framework_simplejwt/views.py
+++ rest_framework_simplejwt/views.py
@@ -1,8 +1,10 @@
+from typing import Optional, Type
 from django.utils.module_loading import import_string
 from rest_framework import generics, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
+from rest_framework import serializers
 
 from .authentication import AUTH_HEADER_TYPES
 from .exceptions import InvalidToken, TokenError
@@ -13,7 +15,7 @@ class TokenViewBase(generics.GenericAPIView):
     permission_classes = ()
     authentication_classes = ()
 
-    serializer_class = None
+    serializer_class: Optional[Type[serializers.Serializer]] = None
     _serializer_class = ""
 
     www_authenticate_realm = "api"
