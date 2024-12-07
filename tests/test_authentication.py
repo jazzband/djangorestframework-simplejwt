7789c87a9fb4db0cbef2a974110f7a0b50c7e678
zxkeyy <z_selmani@estin.dz>
2024-12-07 18:02:12 +0100

    Add test for retrieving inactive user in JWT authentication

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -162,6 +162,31 @@ class TestJWTAuthentication(TestCase):
         self.assertEqual(self.backend.get_user(payload).id, u.id)
 
     @override_api_settings(
+        CHECK_USER_IS_ACTIVE=False,
+    )
+    def test_get_inactive_user(self):
+        payload = {"some_other_id": "foo"}
+
+        # Should raise error if no recognizable user identification
+        with self.assertRaises(InvalidToken):
+            self.backend.get_user(payload)
+
+        payload[api_settings.USER_ID_CLAIM] = 42
+
+        # Should raise exception if user not found
+        with self.assertRaises(AuthenticationFailed):
+            self.backend.get_user(payload)
+
+        u = User.objects.create_user(username="markhamill")
+        u.is_active = False
+        u.save()
+
+        payload[api_settings.USER_ID_CLAIM] = getattr(u, api_settings.USER_ID_FIELD)
+
+        # should return correct user
+        self.assertEqual(self.backend.get_user(payload).id, u.id)
+
+    @override_api_settings(
         CHECK_REVOKE_TOKEN=True, REVOKE_TOKEN_CLAIM="revoke_token_claim"
     )
     def test_get_user_with_check_revoke_token(self):
