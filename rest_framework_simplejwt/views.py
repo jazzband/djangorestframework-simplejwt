cfb37bbeb889a3f695187b7227d24abe1cb3311e
Jonathan Hanson <jonathan@jonathan-hanson.org>
2025-03-02 14:25:17 -0800

    The abstract type is BaseSerializer, not Serializer

--- rest_framework_simplejwt/views.py
+++ rest_framework_simplejwt/views.py
@@ -4,7 +4,7 @@ from django.utils.module_loading import import_string
 from rest_framework import generics, status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer
+from rest_framework.serializers import BaseSerializer
 
 from .authentication import AUTH_HEADER_TYPES
 from .exceptions import InvalidToken, TokenError
@@ -15,12 +15,12 @@ class TokenViewBase(generics.GenericAPIView):
     permission_classes = ()
     authentication_classes = ()
 
-    serializer_class: Optional[type[Serializer]] = None
+    serializer_class: Optional[type[BaseSerializer]] = None
     _serializer_class = ""
 
     www_authenticate_realm = "api"
 
-    def get_serializer_class(self) -> type[Serializer]:
+    def get_serializer_class(self) -> type[BaseSerializer]:
         """
         If serializer_class is set, use it directly. Otherwise get the class from settings.
         """
