a9e31532543d616bad9081a58e52aca2becd9360
Ömer Faruk Korkmaz <omer@upsonic.co>
2025-12-29 17:51:49 +0300

    feat: add django 6.0 and python 3.14 support

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
         "Framework :: Django :: 5.2",
+        "Framework :: Django :: 6.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
