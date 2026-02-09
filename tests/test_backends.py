d3ed61da4a308d034b058af62e02b41917d38508
Vjeran Grozdanic <vjeran.grozdanic@gmail.com>
2026-02-09 22:03:25 +0100

    Revert "use well known vectors"
    This reverts commit 689619193c35e35c24bb2ea11fbce73996c703f7.

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -21,8 +21,10 @@ from rest_framework_simplejwt.utils import aware_utcnow, datetime_to_epoch, make
 from tests.keys import (
     ES256_PRIVATE_KEY,
     ES256_PUBLIC_KEY,
-    ES384_JWK,
-    ES512_JWK,
+    ES384_PRIVATE_KEY,
+    ES384_PUBLIC_KEY,
+    ES512_PRIVATE_KEY,
+    ES512_PUBLIC_KEY,
     PRIVATE_KEY,
     PRIVATE_KEY_2,
     PUBLIC_KEY,
@@ -59,14 +61,12 @@ class TestTokenBackend(TestCase):
             "RS256", PRIVATE_KEY, PUBLIC_KEY, AUDIENCE, ISSUER
         )
         self.payload = {"foo": "bar"}
-        es384_private_key = algorithms.ECAlgorithm.from_jwk(ES384_JWK)
-        es512_private_key = algorithms.ECAlgorithm.from_jwk(ES512_JWK)
         self.backends = (
             self.hmac_token_backend,
             self.rsa_token_backend,
             TokenBackend("ES256", ES256_PRIVATE_KEY, ES256_PUBLIC_KEY),
-            TokenBackend("ES384", es384_private_key, es384_private_key.public_key()),
-            TokenBackend("ES512", es512_private_key, es512_private_key.public_key()),
+            TokenBackend("ES384", ES384_PRIVATE_KEY, ES384_PUBLIC_KEY),
+            TokenBackend("ES512", ES512_PRIVATE_KEY, ES512_PUBLIC_KEY),
         )
 
     def test_init(self):
